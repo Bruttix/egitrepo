0f51ea89bf40db41a12cd83134439f3d96beb0d3
Bruttix <bgreene212@gmail.com>
2021-03-19 04:01:44 -0500

    added 2 more lines

--- EgitTest/src/Default/HelloWorld.java
+++ EgitTest/src/Default/HelloWorld.java
@@ -2,5 +2,7 @@ package Default;
 public class HelloWorld{
 	public static void main(String[] args) {
 		System.out.println("Hi World");
+		System.out.println("new line 2 added");
+		System.out.println("new line 3 added");
 	}
 }
